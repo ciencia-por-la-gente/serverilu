bb088de4fa53489637f660f1abe17e8e4daf78de
Victor J. Salazar <vagol942@gmail.com>
2018-09-01 00:28:36 -0500

    first thing

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var cfenv = require("cfenv");
+var bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send("Hello Victorilu")
+})
